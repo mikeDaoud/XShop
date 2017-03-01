cfb41ebd805e93fbf349d0ebba4ed89a4cbf8149
MustafaMahmoudMM <mostafamahmoud1511@gmail.com>
2017-03-02 01:45:12 +0200

    finish AddToCart, RemoveFromCart & UpdateCartItemQuantity

--- XShop/src/main/java/com/jetsmad/xshop/cart/UpdateCartItemQuantity.java
+++ XShop/src/main/java/com/jetsmad/xshop/cart/UpdateCartItemQuantity.java
@@ -5,13 +5,19 @@
  */
 package com.jetsmad.xshop.cart;
 
+import com.jetsmad.xshop.util.beans.CartItem;
+import com.jetsmad.xshop.util.beans.Product;
+import com.jetsmad.xshop.util.beans.SessionAttrs;
+import com.jetsmad.xshop.util.database.DBController;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -29,7 +35,45 @@ public class UpdateCartItemQuantity extends HttpServlet {
 //        2. gets action (increase or decrease) from request
 //        3. if increase -> check stock -> update item on session -> reply with "true" or "false"
 //        4. if decrease -> update item on session -> reply with "true"
-//       
+        PrintWriter out = response.getWriter();
+        String productId = request.getAttribute("productId").toString();
+        String action = request.getAttribute("action").toString();
+        HttpSession session = request.getSession(false);
+        if(productId != null && action != null && session != null){
+            DBController dbc = new DBController();
+            Product product = dbc.getProduct(productId);
+            int newStock;
+            ArrayList<CartItem> cartItems = (ArrayList<CartItem>) session.getAttribute(SessionAttrs.CART_ITEMS);
+            if(action.equals("increase")){
+                if(product.getStock() >= 1){
+                    for(CartItem item:cartItems){
+                        if(item.getProduct().getId().equals(productId)){
+                            item.addOne();
+                            newStock = product.getStock() - 1;
+                            product.setStock(newStock);
+                            out.write("True");
+                        }
+                    }
+                }else{
+                    out.write("False");
+                }
+            }
+            if(action.equals("decrease")){
+                for(CartItem item:cartItems){
+                    if(item.getProduct().getId().equals(productId)){
+                        if(item.removeOne()){
+                            newStock = product.getStock() + 1;
+                            product.setStock(newStock);
+                            out.write("True");
+                        }
+                    }
+                }
+            }
+            dbc.updateProduct(product);
+            session.setAttribute(SessionAttrs.CART_ITEMS, cartItems);
+        }else{
+            out.write("False");
+        }
     }
 
     @Override
