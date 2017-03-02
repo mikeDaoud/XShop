21517f46113bcf083bd1962d534eab2e99b98bbb
markoiti <markoiti@markoiti-Lenovo-G580>
2017-03-02 19:45:13 +0200

    admin view servlets added admin servlets from implementation

--- XShop/src/main/java/com/jetsmad/xshop/admin/AddNewProduct.java
+++ XShop/src/main/java/com/jetsmad/xshop/admin/AddNewProduct.java
@@ -3,13 +3,10 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.jetsmad.xshop.services;
+package com.jetsmad.xshop.admin;
 
-import com.jetsmad.xshop.util.beans.Product;
-import com.jetsmad.xshop.util.beans.SessionAttrs;
-import com.jetsmad.xshop.util.database.DBController;
 import java.io.IOException;
-import java.util.Vector;
+import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -18,27 +15,35 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  *
- * @author michael
+ * @author markoiti
  */
-@WebServlet(name = "getProducts", urlPatterns = {"/productslist"})
-public class GetProducts extends HttpServlet {
+@WebServlet(name = "AddNewProduct", urlPatterns = {"/AddNewProduct"})
+public class AddNewProduct extends HttpServlet {
 
-// get param "type" from request
-// if equals "all" -> check parameter "pagenum" and get products at this page
-// if equals "category" check parameter "categoryName" and get products of this category
-// if equals "search" check parameters "query" "min" "max" and get products with this search query
-    
-    
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        DBController db = new DBController();
-        Vector<Product> products = db.getAllProducts();
-        
-        request.setAttribute(SessionAttrs.PRODUCTS_LIST, products);
-        
-        request.getRequestDispatcher("products.jsp").include(request,response);
-        
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet AddNewProduct</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet AddNewProduct at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
