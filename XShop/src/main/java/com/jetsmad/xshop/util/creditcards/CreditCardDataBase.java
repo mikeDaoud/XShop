83a2ef0903a2758f73a045bd456e73bd2e0623dc
markoiti <markoiti@markoiti-Lenovo-G580>
2017-03-03 16:37:20 +0200

    Payment classes for implementation Added 2 packages (com.jetsmad.xshop.payment) & (com.jetsmad.xshop.util.creditcards)

---
+++ XShop/src/main/java/com/jetsmad/xshop/util/creditcards/CreditCardDataBase.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jetsmad.xshop.util.creditcards;
+
+/**
+ *
+ * @author markoiti
+ */
+public class CreditCardDataBase {
+    //connect method
+    //disconnect method
+    
+    //validateCreditCard (CredictCard)
+    //checkBalance (credit card number)
+    //processPayment(credit card number, amount)
+    
+}
