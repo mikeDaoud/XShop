95a62e9185cb45bb9429a78a64568396617c29c9
markoiti <markoiti@markoiti-Lenovo-G580>
2017-03-06 00:08:43 +0200

    placeorder and check out implementation not tested

---
+++ WebApplication1/src/java/Upload.java
@@ -0,0 +1,125 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Iterator;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+/**
+ *
+ * @author mosta
+ */
+@WebServlet(urlPatterns = {"/uploadFile"})
+@MultipartConfig
+public class Upload extends HttpServlet {
+private static final String SAVE_DIR = "uploadFiles";
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+try {
+    String path = request.getServletContext().getRealPath("");
+
+
+            // Create a factory for disk-based file items
+            DiskFileItemFactory factory = new DiskFileItemFactory();
+// Create a new file upload handler
+            ServletFileUpload upload = new ServletFileUpload(factory);
+// Parse the request
+            List<FileItem> items = upload.parseRequest(request);
+            Iterator<FileItem> iter = items.iterator();
+            while (iter.hasNext()) {
+                FileItem item = iter.next();
+                if (item.isFormField()) {
+//processFormField(item);
+                    String name = item.getFieldName();
+                    String value = item.getString();
+                    System.out.println(name+"---"+value);
+                } else {
+// processUploadedFile(item);
+                    if (!item.isFormField()) {
+                        item.write(new File(path+"/uploadFiles/"+item.getName()));
+                        System.out.println(path);
+
+                    }
+                }
+            }
+        } catch (Exception ex) {
+        }        
+   
+    }
+    
+    private String extractFileName(Part part) {
+        String contentDisp = part.getHeader("content-disposition");
+        String[] items = contentDisp.split(";");
+        for (String s : items) {
+            System.out.println(s);
+            if (s.trim().startsWith("filename")) {
+                return s.substring(s.indexOf("=") + 2, s.length()-1);
+            }
+        }
+        return "";
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ WebApplication1/web/upload.jsp
@@ -0,0 +1,27 @@
+<%-- 
+    Document   : upload
+    Created on : Mar 5, 2017, 5:46:27 AM
+    Author     : mosta
+--%>
+
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+    "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+        <title>File Upload</title>
+    </head>
+    <body>
+    <center>
+        <h1>File Upload</h1>
+        <form method="post" action="uploadFile"
+              enctype="multipart/form-data">
+            <input name="inputty" type="text"/>
+            Select file to upload: <input type="file" name="file" size="60" /><br />
+            <br /> <input type="submit" value="Upload" />
+        </form>
+    </center>
+</body>
+</html>
